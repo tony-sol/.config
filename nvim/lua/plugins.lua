d7c15bd281eb5573fd5f4c54588f24e9c40f3fc9
Tony-Sol <tony.kent.nar.earth@gmail.com>
2022-06-23 18:45:40 +0300

    Fix vim colors in tmux, add some tmux configs

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -44,36 +44,36 @@ return require('packer').startup(function()
 			'kyazdani42/nvim-web-devicons'
 		},
 		require('lualine').setup {
-			options = {
+			options           = {
 				icons_enabled        = true,
 				theme                = 'vscode',
-				component_separators = { left = '', right = ''},
-				section_separators   = { left = '', right = ''},
+				component_separators = { left = '', right = '' },
+				section_separators   = { left = '', right = '' },
 				disabled_filetypes   = {},
 				always_divide_middle = true,
 				globalstatus         = false
 			},
-			sections = {
-				lualine_a = {'mode'},
-				lualine_b = {'branch', 'diff', 'diagnostics'},
-				lualine_c = {'filename'},
-				lualine_x = {'encoding', 'fileformat', 'filetype'},
-				lualine_y = {'progress'},
-				lualine_z = {'location'}
+			sections          = {
+				lualine_a = { 'mode' },
+				lualine_b = { 'branch', 'diff', 'diagnostics' },
+				lualine_c = { 'filename' },
+				lualine_x = { 'encoding', 'fileformat', 'filetype' },
+				lualine_y = { 'progress' },
+				lualine_z = { 'location' }
 			},
 			inactive_sections = {
 				lualine_a = {},
 				lualine_b = {},
-				lualine_c = {'filename'},
-				lualine_x = {'location'},
+				lualine_c = { 'filename' },
+				lualine_x = { 'location' },
 				lualine_y = {},
 				lualine_z = {}
 			},
-			tabline    = {
-				lualine_a = {'windows'},
-				lualine_b = {'tabs'}
+			tabline           = {
+				lualine_a = { 'windows' },
+				lualine_b = { 'tabs' }
 			},
-			extensions = {}
+			extensions        = {}
 		}
 	}
 	use {
@@ -103,7 +103,7 @@ return require('packer').startup(function()
 				show_symbol_details    = true,
 				preview_bg_highlight   = 'Pmenu',
 				keymaps                = { -- These keymaps can be a string or a table for multiple keys
-					close          = {"<Esc>", "q"},
+					close          = { "<Esc>", "q" },
 					goto_location  = "<Cr>",
 					focus_location = "o",
 					hover_symbol   = "<C-space>",
@@ -114,32 +114,32 @@ return require('packer').startup(function()
 				lsp_blacklist          = {},
 				symbol_blacklist       = {},
 				symbols                = {
-					File          = {icon = ""   , hl = "TSURI"},
-					Module        = {icon = ""   , hl = "TSNamespace"},
-					Namespace     = {icon = ""   , hl = "TSNamespace"},
-					Package       = {icon = ""   , hl = "TSNamespace"},
-					Class         = {icon = "𝓒"   , hl = "TSType"},
-					Method        = {icon = "ƒ"   , hl = "TSMethod"},
-					Property      = {icon = ""   , hl = "TSMethod"},
-					Field         = {icon = ""   , hl = "TSField"},
-					Constructor   = {icon = ""   , hl = "TSConstructor"},
-					Enum          = {icon = "ℰ"   , hl = "TSType"},
-					Interface     = {icon = "ﰮ"   , hl = "TSType"},
-					Function      = {icon = ""   , hl = "TSFunction"},
-					Variable      = {icon = ""   , hl = "TSConstant"},
-					Constant      = {icon = ""   , hl = "TSConstant"},
-					String        = {icon = "𝓐"   , hl = "TSString"},
-					Number        = {icon = "#"   , hl = "TSNumber"},
-					Boolean       = {icon = "⊨"   , hl = "TSBoolean"},
-					Array         = {icon = ""   , hl = "TSConstant"},
-					Object        = {icon = "⦿"   , hl = "TSType"},
-					Key           = {icon = "🔐"  , hl = "TSType"},
-					Null          = {icon = "NULL", hl = "TSType"},
-					EnumMember    = {icon = ""   , hl = "TSField"},
-					Struct        = {icon = "𝓢"   , hl = "TSType"},
-					Event         = {icon = "🗲"   , hl = "TSType"},
-					Operator      = {icon = "+"   , hl = "TSOperator"},
-					TypeParameter = {icon = "𝙏"   , hl = "TSParameter"}
+					File          = { icon = "", hl = "TSURI" },
+					Module        = { icon = "", hl = "TSNamespace" },
+					Namespace     = { icon = "", hl = "TSNamespace" },
+					Package       = { icon = "", hl = "TSNamespace" },
+					Class         = { icon = "𝓒", hl = "TSType" },
+					Method        = { icon = "ƒ", hl = "TSMethod" },
+					Property      = { icon = "", hl = "TSMethod" },
+					Field         = { icon = "", hl = "TSField" },
+					Constructor   = { icon = "", hl = "TSConstructor" },
+					Enum          = { icon = "ℰ", hl = "TSType" },
+					Interface     = { icon = "ﰮ", hl = "TSType" },
+					Function      = { icon = "", hl = "TSFunction" },
+					Variable      = { icon = "", hl = "TSConstant" },
+					Constant      = { icon = "", hl = "TSConstant" },
+					String        = { icon = "𝓐", hl = "TSString" },
+					Number        = { icon = "#", hl = "TSNumber" },
+					Boolean       = { icon = "⊨", hl = "TSBoolean" },
+					Array         = { icon = "", hl = "TSConstant" },
+					Object        = { icon = "⦿", hl = "TSType" },
+					Key           = { icon = "🔐", hl = "TSType" },
+					Null          = { icon = "NULL", hl = "TSType" },
+					EnumMember    = { icon = "", hl = "TSField" },
+					Struct        = { icon = "𝓢", hl = "TSType" },
+					Event         = { icon = "🗲", hl = "TSType" },
+					Operator      = { icon = "+", hl = "TSOperator" },
+					TypeParameter = { icon = "𝙏", hl = "TSParameter" }
 				}
 			}
 		end
@@ -155,7 +155,7 @@ return require('packer').startup(function()
 		}
 	}
 	use {
-		'hrsh7th/nvim-cmp',	-- completion
+		'hrsh7th/nvim-cmp', -- completion
 		require('cmp').setup {
 			mapping = {
 				['<Tab>'] = function(fallback)
@@ -205,7 +205,7 @@ return require('packer').startup(function()
 	use {
 		'kyazdani42/nvim-tree.lua',
 		requires = {
-			'kyazdani42/nvim-web-devicons'	-- optional, for file icon
+			'kyazdani42/nvim-web-devicons' -- optional, for file icon
 		},
 		require('nvim-tree').setup {
 			auto_reload_on_write               = true,
@@ -219,7 +219,7 @@ return require('packer').startup(function()
 			open_on_tab                        = false,
 			sort_by                            = 'name',
 			update_cwd                         = false,
-			view = {
+			view                               = {
 				width                       = 30,
 				height                      = 30,
 				hide_root_folder            = false,
@@ -228,14 +228,14 @@ return require('packer').startup(function()
 				number                      = false,
 				relativenumber              = false,
 				signcolumn                  = 'yes',
-				mappings = {
+				mappings                    = {
 					custom_only = false,
 					list = {
-					--	user mappings go here
+						--	user mappings go here
 					}
 				}
 			},
-			renderer = {
+			renderer                           = {
 				indent_markers = {
 					enable = true,
 					icons = {
@@ -249,41 +249,41 @@ return require('packer').startup(function()
 					git_placement = 'before'
 				}
 			},
-			hijack_directories = {
+			hijack_directories                 = {
 				enable    = true,
 				auto_open = true
 			},
-			update_focused_file = {
+			update_focused_file                = {
 				enable      = false,
 				update_cwd  = false,
 				ignore_list = {}
 			},
-			ignore_ft_on_setup = {},
-			system_open = {
+			ignore_ft_on_setup                 = {},
+			system_open                        = {
 				cmd  = '',
 				args = {}
 			},
-			diagnostics = {
+			diagnostics                        = {
 				enable       = false,
 				show_on_dirs = false,
-				icons = {
+				icons        = {
 					hint    = '',
 					info    = '',
 					warning = '',
 					error   = ''
 				}
 			},
-			filters = {
+			filters                            = {
 				dotfiles = false,
 				custom   = {},
 				exclude  = {}
 			},
-			git = {
+			git                                = {
 				enable  = true,
 				ignore  = false,
 				timeout = 400
 			},
-			actions = {
+			actions                            = {
 				use_system_clipboard = true,
 				change_dir = {
 					enable             = true,
@@ -303,14 +303,14 @@ return require('packer').startup(function()
 					}
 				}
 			},
-			trash = {
+			trash                              = {
 				cmd             = 'trash',
 				require_confirm = true
 			},
-			log = {
+			log                                = {
 				enable   = false,
 				truncate = false,
-				types = {
+				types    = {
 					all         = false,
 					config      = false,
 					copy_paste  = false,
@@ -334,23 +334,23 @@ return require('packer').startup(function()
 		},
 		config = function()
 			require('gitsigns').setup {
-				signs = {
-					add          = {hl = 'GitSignsAdd'   , text = '│', numhl='GitSignsAddNr'   , linehl='GitSignsAddLn'},
-					change       = {hl = 'GitSignsChange', text = '│', numhl='GitSignsChangeNr', linehl='GitSignsChangeLn'},
-					delete       = {hl = 'GitSignsDelete', text = '_', numhl='GitSignsDeleteNr', linehl='GitSignsDeleteLn'},
-					topdelete    = {hl = 'GitSignsDelete', text = '‾', numhl='GitSignsDeleteNr', linehl='GitSignsDeleteLn'},
-					changedelete = {hl = 'GitSignsChange', text = '~', numhl='GitSignsChangeNr', linehl='GitSignsChangeLn'},
+				signs                             = {
+					add          = { hl = 'GitSignsAdd', text = '│', numhl = 'GitSignsAddNr', linehl = 'GitSignsAddLn' },
+					change       = { hl = 'GitSignsChange', text = '│', numhl = 'GitSignsChangeNr', linehl = 'GitSignsChangeLn' },
+					delete       = { hl = 'GitSignsDelete', text = '_', numhl = 'GitSignsDeleteNr', linehl = 'GitSignsDeleteLn' },
+					topdelete    = { hl = 'GitSignsDelete', text = '‾', numhl = 'GitSignsDeleteNr', linehl = 'GitSignsDeleteLn' },
+					changedelete = { hl = 'GitSignsChange', text = '~', numhl = 'GitSignsChangeNr', linehl = 'GitSignsChangeLn' },
 				},
-				signcolumn = true,	-- Toggle with `:Gitsigns toggle_signs`
-				numhl      = true,	-- Toggle with `:Gitsigns toggle_numhl`
-				linehl     = false,	-- Toggle with `:Gitsigns toggle_linehl`
-				word_diff  = true,	-- Toggle with `:Gitsigns toggle_word_diff`
-				keymaps = {
+				signcolumn                        = true, -- Toggle with `:Gitsigns toggle_signs`
+				numhl                             = true, -- Toggle with `:Gitsigns toggle_numhl`
+				linehl                            = false, -- Toggle with `:Gitsigns toggle_linehl`
+				word_diff                         = true, -- Toggle with `:Gitsigns toggle_word_diff`
+				keymaps                           = {
 					-- Default keymap options
 					noremap = true,
 
-					['n ]c'] = { expr = true, "&diff ? ']c' : '<cmd>Gitsigns next_hunk<CR>'"},
-					['n [c'] = { expr = true, "&diff ? '[c' : '<cmd>Gitsigns prev_hunk<CR>'"},
+					['n ]c'] = { expr = true, "&diff ? ']c' : '<cmd>Gitsigns next_hunk<CR>'" },
+					['n [c'] = { expr = true, "&diff ? '[c' : '<cmd>Gitsigns prev_hunk<CR>'" },
 
 					['n <leader>hs'] = '<cmd>Gitsigns stage_hunk<CR>',
 					['v <leader>hs'] = ':Gitsigns stage_hunk<CR>',
@@ -367,33 +367,33 @@ return require('packer').startup(function()
 					['o ih'] = ':<C-U>Gitsigns select_hunk<CR>',
 					['x ih'] = ':<C-U>Gitsigns select_hunk<CR>'
 				},
-				watch_gitdir = {
+				watch_gitdir                      = {
 					interval     = 1000,
 					follow_files = true
 				},
-				attach_to_untracked     = true,
-				current_line_blame      = true,	-- Toggle with `:Gitsigns toggle_current_line_blame`
-				current_line_blame_opts = {
+				attach_to_untracked               = true,
+				current_line_blame                = true, -- Toggle with `:Gitsigns toggle_current_line_blame`
+				current_line_blame_opts           = {
 					virt_text         = true,
-					virt_text_pos     = 'eol',	-- 'eol' | 'overlay' | 'right_align'
+					virt_text_pos     = 'eol', -- 'eol' | 'overlay' | 'right_align'
 					delay             = 1000,
 					ignore_whitespace = false,
 				},
 				current_line_blame_formatter_opts = {
 					relative_time = false
 				},
-				sign_priority    = 6,
-				update_debounce  = 100,
-				status_formatter = nil,	-- Use default
-				max_file_length  = 40000,
-				preview_config   = {
+				sign_priority                     = 6,
+				update_debounce                   = 100,
+				status_formatter                  = nil, -- Use default
+				max_file_length                   = 40000,
+				preview_config                    = {
 					border   = 'single',
 					style    = 'minimal',
 					relative = 'cursor',
 					row      = 0,
 					col      = 1
 				},
-				yadm = {
+				yadm                              = {
 					enable = false
 				}
 			}
