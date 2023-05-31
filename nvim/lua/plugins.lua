1572295e17af7567df0e5010baa60f28ba02a0e9
Tony-Sol <tony.kent.nar.earth@gmail.com>
2023-05-31 17:58:45 +0300

    Add tmux-fzf-url

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -276,6 +276,13 @@ require('packer').startup(function (use)
 		config   = function ()
 			local noice = require('noice')
 			noice.setup {
+				lsp = {
+					override = {
+						['vim.lsp.util.convert_input_to_markdown_lines'] = true,
+						['vim.lsp.util.stylize_markdown']                = true,
+						['cmp.entry.get_documentation']                  = true,
+					},
+				},
 				presets = {
 					bottom_search   = false,
 					lsp_doc_border  = true,
