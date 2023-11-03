3fa652ab950dd77777d815dfd4ab62712e7e9a0c
Tony-Sol <tony.kent.nar.earth@gmail.com>
2023-11-03 03:39:07 +0300

    wezterm: new color schemes

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -5,17 +5,26 @@ local function get_appearance()
 	return wezterm.gui and wezterm.gui.get_appearance() or 'Dark'
 end
 
+---@param family string
 ---@param appearance string
----@return 'vscode_dark' | 'vscode_light'
-local function scheme_for_appearance(appearance)
-	return appearance:find('Dark') and 'vscode_dark' or 'vscode_light'
+---@return string
+local function scheme_for_appearance(family, appearance)
+	local switch = {
+		['vscode'] = function(_appearance)
+			return _appearance:find('Dark') and 'vscode_dark' or 'vscode_light'
+		end,
+		['tokyonight'] = function(_appearance)
+			return _appearance:find('Dark') and 'tokyonight_night' or 'tokyonight_day'
+		end,
+	}
+	return switch[family](appearance)
 end
 
 return {
 	term                         = 'xterm-256color',
 	default_prog                 = { '/bin/zsh', '--login', '--interactive', '-c', 'tmux new -A' },
 	exit_behavior                = 'CloseOnCleanExit',
-	color_scheme                 = scheme_for_appearance(get_appearance()),
+	color_scheme                 = scheme_for_appearance('vscode', get_appearance()),
 	color_schemes                = {
 		vscode_dark = {
 			background = '#1E1E1E',
@@ -63,6 +72,130 @@ return {
 				'#C451CE', -- magenta
 				'#52A8C7', -- cyan
 				'#A6A3A6', -- white
+			},
+		},
+		tokyonight_day = {
+			background     = '#E1E2E7',
+			foreground     = '#3760BF',
+			cursor_bg      = '#3760BF',
+			cursor_border  = '#3760BF',
+			cursor_fg      = '#E1E2E7',
+			selection_bg   = '#B6BFE2',
+			selection_fg   = '#3760BF',
+			split          = '#2E7DE9',
+			compose_cursor = '#B15C00',
+			ansi           = {
+				'#E9E9ED', -- black
+				'#F52A65', -- red
+				'#587539', -- green
+				'#8C6C3E', -- yellow
+				'#2E7DE9', -- blue
+				'#9854F1', -- magenta
+				'#007197', -- cyan
+				'#6172B0', -- white
+			},
+			brights        = {
+				'#A1A6C5', -- black
+				'#F52A65', -- red
+				'#587539', -- green
+				'#8C6C3E', -- yellow
+				'#2E7DE9', -- blue
+				'#9854F1', -- magenta
+				'#007197', -- cyan
+				'#3760BF', -- white
+			},
+		},
+		tokyonight_moon = {
+			foreground     = '#C8D3F5',
+			background     = '#222436',
+			cursor_bg      = '#C8D3F5',
+			cursor_border  = '#C8D3F5',
+			cursor_fg      = '#222436',
+			selection_bg   = '#2D3F76',
+			selection_fg   = '#C8D3F5',
+			split          = '#82AAFF',
+			compose_cursor = '#FF966C',
+			ansi           = {
+				'#1B1D2B', -- black
+				'#FF757F', -- red
+				'#C3E88D', -- green
+				'#FFC777', -- yellow
+				'#82AAFF', -- blue
+				'#C099FF', -- magenta
+				'#86E1FC', -- cyan
+				'#828BB8', -- white
+			},
+			brights        = {
+				'#444A73', -- black
+				'#FF757F', -- red
+				'#C3E88D', -- green
+				'#FFC777', -- yellow
+				'#82AAFF', -- blue
+				'#C099FF', -- magenta
+				'#86E1FC', -- cyan
+				'#C8D3F5', -- white
+			},
+		},
+		tokyonight_night = {
+			foreground     = '#C0CAF5',
+			background     = '#1A1B26',
+			cursor_bg      = '#C0CAF5',
+			cursor_border  = '#C0CAF5',
+			cursor_fg      = '#1A1B26',
+			selection_bg   = '#283457',
+			selection_fg   = '#C0CAF5',
+			split          = '#7AA2F7',
+			compose_cursor = '#FF9E64',
+			ansi           = {
+				'#15161E', -- black
+				'#F7768E', -- red
+				'#9ECE6A', -- green
+				'#E0AF68', -- yellow
+				'#7AA2F7', -- blue
+				'#BB9AF7', -- magenta
+				'#7DCFFF', -- cyan
+				'#A9B1D6', -- white
+			},
+			brights        = {
+				'#414868', -- black
+				'#F7768E', -- red
+				'#9ECE6A', -- green
+				'#E0AF68', -- yellow
+				'#7AA2F7', -- blue
+				'#BB9AF7', -- magenta
+				'#7DCFFF', -- cyan
+				'#C0CAF5', -- white
+			},
+		},
+		tokyonight_storm = {
+			foreground     = '#C0CAF5',
+			background     = '#24283B',
+			cursor_bg      = '#C0CAF5',
+			cursor_border  = '#C0CAF5',
+			cursor_fg      = '#24283B',
+			selection_bg   = '#2E3C64',
+			selection_fg   = '#C0CAF5',
+			split          = '#7AA2F7',
+			compose_cursor = '#FF9E64',
+			ansi           = {
+				'#1D202F', -- black
+				'#F7768E', -- red
+				'#9ECE6A', -- green
+				'#E0AF68', -- yellow
+				'#7AA2F7', -- blue
+				'#BB9AF7', -- magenta
+				'#7DCFFF', -- cyan
+				'#A9B1D6', -- white
+			},
+			brights        = {
+				'#414868', -- black
+				'#F7768E', -- red
+				'#9ECE6A', -- green
+				'#E0AF68', -- yellow
+				'#7AA2F7', -- blue
+				'#BB9AF7', -- magenta
+				'#7DCFFF', -- cyan
+				'#C0CAF5', -- white
 			},
 		},
 	},
