acd10847e7afa75bdc3167d5cfef427e63c42e54
Tony-Sol <tony.kent.nar.earth@gmail.com>
2024-01-10 15:56:46 +0300

    nvim: update ufo settings

--- nvim/lua/plugins/nvim-ufo.lua
+++ nvim/lua/plugins/nvim-ufo.lua
@@ -6,20 +6,9 @@ return {
 		},
 	},
 	config       = function()
+		local fn      = vim.fn
 		local promise = require('promise')
 		local ufo     = require('ufo')
-		local function customizeSelector(bufnr)
-			return ufo.getFolds(bufnr, 'lsp')
-				:catch(function(err)
-					return ufo.getFolds(bufnr, 'treesitter')
-				end)
-				:catch(function(err)
-					return ufo.getFolds(bufnr, 'indent')
-				end)
-				:catch(function(err)
-					return promise.reject(err)
-				end)
-		end
 		ufo.setup {
 			preview = {
 				win_config = {
@@ -34,7 +23,45 @@ return {
 				}
 			},
 			provider_selector = function(bufnr, filetype, buftype)
-				return customizeSelector
+				return function(_bufnr)
+					return ufo.getFolds(_bufnr, 'lsp')
+						:catch(function(err)
+							return ufo.getFolds(_bufnr, 'treesitter')
+						end)
+						:catch(function(err)
+							return ufo.getFolds(_bufnr, 'indent')
+						end)
+						:catch(function(err)
+							return promise.reject(err)
+						end)
+				end
+			end,
+			fold_virt_text_handler = function(virtText, lnum, endLnum, width, truncate)
+				local newVirtText = {}
+				local suffix      = (' 󰁂 %d '):format(endLnum - lnum)
+				local sufWidth    = fn.strdisplaywidth(suffix)
+				local targetWidth = width - sufWidth
+				local curWidth    = 0
+				for _, chunk in ipairs(virtText) do
+					local chunkText  = chunk[1]
+					local chunkWidth = fn.strdisplaywidth(chunkText)
+					if targetWidth > curWidth + chunkWidth then
+						table.insert(newVirtText, chunk)
+					else
+						chunkText = truncate(chunkText, targetWidth - curWidth)
+						local hlGroup = chunk[2]
+						table.insert(newVirtText, { chunkText, hlGroup })
+						chunkWidth = fn.strdisplaywidth(chunkText)
+						-- str width returned from truncate() may less than 2nd argument, need padding
+						if curWidth + chunkWidth < targetWidth then
+							suffix = suffix .. (' '):rep(targetWidth - curWidth - chunkWidth)
+						end
+						break
+					end
+					curWidth = curWidth + chunkWidth
+				end
+				table.insert(newVirtText, { suffix })
+				return newVirtText
 			end,
 		}
 	end
