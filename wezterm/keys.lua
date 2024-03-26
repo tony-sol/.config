dce7ed8966c81b5b417053b91416b9f3967df012
Tony-Sol <tony.kent.nar.earth@gmail.com>
2024-03-26 20:14:14 +0300

    wezterm: config ref

---
+++ wezterm/keys.lua
@@ -0,0 +1,15 @@
+local actions = require('wezterm').action
+
+return {
+	{ key = 'q', mods = 'SUPER',      action = actions.QuitApplication },
+	{ key = 'h', mods = 'SUPER',      action = actions.HideApplication },
+	{ key = '0', mods = 'SUPER',      action = actions.ResetFontSize },
+	{ key = '-', mods = 'SUPER',      action = actions.DecreaseFontSize },
+	{ key = '=', mods = 'SUPER',      action = actions.IncreaseFontSize },
+	{ key = 'c', mods = 'SUPER',      action = actions.CopyTo 'Clipboard' },
+	{ key = 'v', mods = 'SUPER',      action = actions.PasteFrom 'Clipboard' },
+	{ key = ' ', mods = 'SHIFT|CTRL', action = actions.QuickSelect },
+	{ key = '?', mods = 'SHIFT|CTRL', action = actions.ShowLauncher },
+	{ key = '|', mods = 'SHIFT|CTRL', action = actions.ShowDebugOverlay },
+	{ key = 'f', mods = 'SUPER|CTRL', action = actions.ToggleFullScreen },
+}
