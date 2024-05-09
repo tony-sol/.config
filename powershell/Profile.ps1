f95966bfe9c36d5e26e5833f22efc3e89875960e
Tony-Sol <tony.kent.nar.earth@gmail.com>
2024-05-09 17:25:26 +0300

    powershell: update profile

--- powershell/Profile.ps1
+++ powershell/Profile.ps1
@@ -1,41 +1,28 @@
 $env:SHELL = '/usr/local/bin/pwsh'
 
+. "$PSScriptRoot\Utils.ps1"
+
 Import-Module Terminal-Icons
-
-Import-Module posh-git
-Function global:PromptWriteErrorInfo() {
-	if ($global:GitPromptValues.DollarQuestion) {
-		Write-Prompt " ✔︎ " -ForegroundColor ([ConsoleColor]::Green)
-	} else {
-		Write-Prompt " ✘ " -ForegroundColor ([ConsoleColor]::Red)
-	}
-}
-$global:GitPromptSettings.DefaultPromptPath.ForegroundColor = 'Orange'
-$global:GitPromptSettings.DefaultPromptBeforeSuffix.Text = '$(PromptWriteErrorInfo)'
-
-Import-Module posh-with
-
-Import-Module CompletionPredictor
-
-Import-Module Microsoft.PowerShell.UnixCompleters
-Set-UnixCompleter -ShellType Zsh
-
 Import-Module PSReadLine
-Set-PSReadLineKeyHandler -Key Tab -Function MenuComplete
-$PSReadLineOptions = @{
-	HistoryNoDuplicates = $True
-	HistorySearchCursorMovesToEnd = $True
-	PredictionSource = "HistoryAndPlugin"
-}
-Set-PSReadLineOption @PSReadLineOptions
+	Set-PSReadLineKeyHandler -Key Tab -Function MenuComplete
+	Set-PSReadLineOption -ShowToolTips:$true
+	Set-PSReadLineOption -HistoryNoDuplicates:$true
+	Set-PSReadLineOption -HistorySearchCursorMovesToEnd:$true
+	Set-PSReadLineOption -PredictionSource:"HistoryAndPlugin"
+	# @FIX Commands list not working in Vi-mode on tab press
+	Set-PSReadlineOption -EditMode "Vi"
+	Set-PSReadLineOption -ViModeIndicator Script
+	# @FIX On startup, cursor is always system
+	Set-PSReadLineOption -ViModeChangeHandler $Function:_onViModeChange
+Import-Module CompletionPredictor
+Import-Module ps-menu
+Import-Module posh-git
+	$global:GitPromptSettings.DefaultPromptPath.ForegroundColor = 'Orange'
+	$global:GitPromptSettings.DefaultPromptBeforeSuffix.Text = '$(PromptWriteErrorInfo)'
+Import-Module Microsoft.PowerShell.SecretManagement
+Import-Module Microsoft.PowerShell.SecretStore
 
-Function _vim() {
-	$env:SHELL = '/bin/zsh'
-	nvim $args
-}
-Set-Alias -Name vim -Value _vim -Option AllScope
-
-Function _ll() {
-	Get-ChildItem -Force $args
-}
-Set-Alias -Name ll -Value _ll -Option AllScope
+Set-Alias -Name vim -Value _nvim -Option AllScope
+Set-Alias -Name nvim -Value _nvim -Option AllScope
+Set-Alias -Name t -Value _t -Option AllScope
+Set-Alias -Name tt -Value _tt -Option AllScope
