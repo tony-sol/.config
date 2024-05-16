ff866daac78d4dea164dba1ae49eec9003e958ff
Tony-Sol <tony.kent.nar.earth@gmail.com>
2024-05-16 15:55:28 +0300

    nvim: add keybindings

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -43,6 +43,18 @@ keymap.set(
 	{ desc = 'Toggle softwrap' }
 )
 keymap.set(
+	{ 'v' },
+	'K',
+	":m '<-2<CR>gv=gv",
+	{ desc = 'Move line up' }
+)
+keymap.set(
+	{ 'v' },
+	'J',
+	":m '>+1<CR>gv=gv",
+	{ desc = 'Move line down' }
+)
+keymap.set(
 	{ 'n' },
 	'zp',
 	function()
