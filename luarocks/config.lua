824029db0f925bc053e43824be11d4950fc42116
Tony-Sol <tony.kent.nar.earth@gmail.com>
2025-02-17 01:19:21 +0300

    luarocks: update config

--- luarocks/config.lua
+++ luarocks/config.lua
@@ -3,5 +3,5 @@
 home_tree       = os_getenv("XDG_DATA_HOME") .. "/luarocks";
 rocks_trees     = {
 	{ name = "user",   root = home_tree };
-	{ name = "system", root = os_getenv("LUAROCKS_HOME") };
+	{ name = "system", root = os_getenv("LUAROCKS_HOME") or home_tree };
 }
