1e186b94f0c7d4119b93672b3d8fc650b413a1b2
Tony-Sol <tony.kent.nar.earth@gmail.com>
2025-06-10 17:00:01 +0300

    hyprland: init

---
+++ hypr/scripts/systemctl.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# systemctl restart --user xdg-desktop-portal.service
+
+# gsettings set org.gnome.desktop.interface color-scheme "prefer-dark"
+# gsettings set org.gnome.desktop.interface gtk-theme "Breeze-Dark
+
+# sleep 1
+
+# # kill all possible running xdg-desktop-portals
+# killall xdg-desktop-portal-hyprland
+# killall xdg-desktop-portal-gnome
+# killall xdg-desktop-portal-kde
+# killall xdg-desktop-portal-lxqt
+# killall xdg-desktop-portal-wlr
+# killall xdg-desktop-portal-gtk
+# killall xdg-desktop-portal
+# sleep 1
+
+# # start xdg-desktop-portal-hyprland
+# /usr/lib/xdg-desktop-portal-hyprland &
+# sleep 2
+
+# # start xdg-desktop-portal
+# /usr/lib/xdg-desktop-portal &
+# sleep 1
