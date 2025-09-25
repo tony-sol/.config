05c4f8cb269ad02068f240840f7c5650c451245c
Tony-Sol <tony.kent.nar.earth@gmail.com>
2025-09-25 04:11:04 +0300

    nvim: update configs

---
+++ nvim/lua/plugins/blink-pairs.lua
@@ -0,0 +1,14 @@
+return {
+	-- 'saghen/blink.pairs',
+	-- version      = '*',
+	-- dependencies = {
+	-- 	{
+	-- 		'saghen/blink.download',
+	-- 	},
+	-- },
+	-- opts         = {
+	-- 	highlights = {
+	-- 		enabled = false,
+	-- 	},
+	-- }
+}
